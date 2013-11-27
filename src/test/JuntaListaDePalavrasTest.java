1ebfb5b7ad70429ebe5f04b741130b25008f6315
Ezequely <ezequelyfreitas@gmail.com>
2013-11-27 21:48:31 -0200

    Create JuntaListaDePalavrasTest.java

---
+++ src/test/JuntaListaDePalavrasTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.*;
+import main.java.jogo.JuntaListasDePalavras;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JuntaListasDePalavrasTest {
+
+	private JuntaListasDePalavras junta;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testJuntarListas() {
+		assertNull("Sucesso", junta);
+	}
+
+}
