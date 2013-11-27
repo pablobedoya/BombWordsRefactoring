f4c53fad8fab31d1999fcc2b64152462126f409d
Ezequely <ezequelyfreitas@gmail.com>
2013-11-27 21:46:58 -0200

    Create ConcreteDAOPalavrasDeListaArquivosTest.java

---
+++ src/test/ConcreteDAOPalavrasDeListaArquivosTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import main.java.dao.ConcreteDAOPalavrasDeListaArquivos;
+import main.java.jogo.PalavrasETraducoes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConcreteDAOPalavrasDeListaArquivosTest {
+
+	private ConcreteDAOPalavrasDeListaArquivos plv = new ConcreteDAOPalavrasDeListaArquivos();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void testExtrairPalavrasETraducoes() {
+		LinkedList<String> palavrasDoJogo = new LinkedList<String>();
+		palavrasDoJogo.add("Table");
+		LinkedList<String> traducoesDoJogo = new LinkedList<String>();
+		traducoesDoJogo.add("mesa");
+		PalavrasETraducoes resp = new PalavrasETraducoes(palavrasDoJogo, traducoesDoJogo);
+		assertNotEquals(resp, plv.extrairPalavrasETraducoes("palavras"));
+	}
+
+	@Test
+	public void testAdicionarNovaPalavraNaLista() {
+		assertTrue(plv.adicionarNovaPalavraNaLista("palavras", "Vasco da Gama", "Maior do Mundo"));
+	}
+
+	@Test
+	public void testRemoverPalavraDaLista() {
+		
+	}
+
+}
