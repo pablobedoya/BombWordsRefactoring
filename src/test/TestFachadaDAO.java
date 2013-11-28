66474b57123569c5dc92fd066c11d2b6d382e590
Ezequely <ezequelyfreitas@gmail.com>
2013-11-27 22:25:01 -0200

    Create TestFachadaDAO.java

---
+++ src/test/TestFachadaDAO.java
@@ -0,0 +1,91 @@
+package test;
+
+import static org.junit.Assert.*;
+import main.java.fachadaparadao.FachadaDAO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFachadaDAO {
+
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetFachadaDAO() {
+		assertNotNull(FachadaDAO.getFachadaDAO());
+	}
+
+	@Test
+	public void testLerConfiguracoesJogo() {
+		
+	}
+
+	@Test
+	public void testSetarNovasConfiguracoesJogo() {
+		
+	}
+
+	@Test
+	public void testCriarNovaListaPalavras() {
+		assertTrue(FachadaDAO.getFachadaDAO().criarNovaListaPalavras("novaLista"));
+	}
+
+	@Test
+	public void testRemoverListaPalavras() {
+		assertFalse(FachadaDAO.getFachadaDAO().removerListaPalavras("naoExiste"));
+	}
+
+	@Test
+	public void testLimparListaPalavras() {
+		assertTrue(FachadaDAO.getFachadaDAO().limparListaPalavras("novaLista"));
+	}
+
+	@Test
+	public void testExtrairNomesDeTodasAsListasDePalavras() {
+
+	}
+
+	@Test
+	public void testListaDePalavrasJahExiste() {
+		assertTrue(FachadaDAO.getFachadaDAO().listaDePalavrasJahExiste("novaLista"));
+	}
+
+	@Test
+	public void testMudarListaASerUsadaNoJogo() {
+		
+	}
+
+	@Test
+	public void testPegarNomeListaASerUsadaNoJogo() {
+		
+	}
+
+	@Test
+	public void testExtrairPalavrasETraducoes() {
+		
+	}
+
+	@Test
+	public void testAdicionarNovaPalavraNaLista() {
+		FachadaDAO.getFachadaDAO().adicionarNovaPalavraNaLista("novaLista", "Vasco da Gama", "Maior do Mundo");
+	}
+
+	@Test
+	public void testRemoverPalavraDaLista() {
+		
+	}
+
+	@Test
+	public void testInserirJogadorRanking() {
+		
+	}
+
+	@Test
+	public void testLerRanking() {
+		
+	}
+
+}
