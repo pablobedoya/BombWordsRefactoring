9193ae9023eda1aff0b7b820291c61c1b7c7f62e
Pablo Bedoya <pablo.reds@hotmail.com>
2013-12-01 21:14:29 -0300

    Testes pacote command

---
+++ src/test/TestConcreteCommandAdicionarPalavraNaLista.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import main.java.command.ConcreteCommandAdicionarPalavraNaLista;
+
+import org.junit.Test;
+
+public class TestConcreteCommandAdicionarPalavraNaLista {
+
+	@Test
+	public void testAdicionarPalavraNaLista1()
+		throws Exception {
+		ConcreteCommandAdicionarPalavraNaLista fixture = new ConcreteCommandAdicionarPalavraNaLista("", "", "");
+
+		boolean result = fixture.adicionarPalavraNaLista();
+
+		assertEquals(false, result);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testDesfazerAdicionarPalavraNaLista()
+		throws Exception {
+		ConcreteCommandAdicionarPalavraNaLista fixture = new ConcreteCommandAdicionarPalavraNaLista("", "", "");
+
+		fixture.desfazerAdicionarPalavraNaLista();
+	}
+
+	@Test
+	public void testGetPalavraAdicionar()
+		throws Exception {
+		ConcreteCommandAdicionarPalavraNaLista fixture = new ConcreteCommandAdicionarPalavraNaLista("", "", "");
+
+		String result = fixture.getPalavraAdicionar();
+
+		assertEquals("", result);
+	}
+	
+}
---
+++ src/test/TestConcreteCommandRemoverPalavrasDaLista.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.LinkedList;
+
+import main.java.command.ConcreteCommandRemoverPalavrasDaLista;
+
+import org.junit.Test;
+
+public class TestConcreteCommandRemoverPalavrasDaLista {
+
+	@Test
+	public void testDesfazerRemocaoPalavrasDaLista()
+		throws Exception {
+		ConcreteCommandRemoverPalavrasDaLista fixture = new ConcreteCommandRemoverPalavrasDaLista(new LinkedList(), new LinkedList(), "");
+
+		fixture.desfazerRemocaoPalavrasDaLista();
+	}
+
+	@Test
+	public void testRemoverPalavrasDaLista()
+		throws Exception {
+		ConcreteCommandRemoverPalavrasDaLista fixture = new ConcreteCommandRemoverPalavrasDaLista(new LinkedList(), new LinkedList(), "");
+
+		fixture.removerPalavrasDaLista();
+	}
+	
+}
